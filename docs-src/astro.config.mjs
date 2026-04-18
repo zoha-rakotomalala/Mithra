a90626f6f4c343e0e2cb155ca0975c8c8c6e35c1
Zoha Rakotomalala <zoha.rakotomalala@gmail.com>
2026-04-18 23:20:15 +0200

    Adding github action to deploy the website

--- docs-src/astro.config.mjs
+++ docs-src/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://zoha-rakotomalala.github.io',
   base: '/Mithra/',
-  outDir: '../docs',
+  outDir: './dist',
   build: { assets: '_assets' },
 });
